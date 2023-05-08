04bfe6fe95597fd8d49c57bf321cdf753b79e252
Soumik Das <114335541+soumik1065@users.noreply.github.com>
2023-05-09 01:14:48 +0530

    Add files via upload

---
+++ 2D_Array_using_Pointers.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void main()
+{
+	int arr[50][50],row,column,i,j;
+
+	printf("Enter the size of row: ");
+	scanf("%d",&row);
+	printf("Enter the size of column: ");
+	scanf("%d",&column);
+
+	printf("\nEnter the elements in the array:\n");
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<column;j++)
+		{
+			scanf("%d",(arr[i]+j));
+		}
+	}
+
+	printf("\nElements in the array are:\n");
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<column;j++)
+		{
+			printf("%d\t",*(arr[i]+j));
+		}
+		printf("\n");
+	}
+}
